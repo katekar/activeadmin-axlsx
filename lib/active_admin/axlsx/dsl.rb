f668556071072f9fddc39a06794d451d75f80f29
Katerina <katerina_karpycheva@mail.ru>
2016-07-19 13:41:00 +0300

    Check if table exists

--- lib/active_admin/axlsx/dsl.rb
+++ lib/active_admin/axlsx/dsl.rb
@@ -4,6 +4,7 @@ module ActiveAdmin
       delegate :ingnore_columns, :column, :after_filer, :i18n_scope, :header_style, :skip_header, :white_list, to: :xlsx_builder, prefix: :config
       # @see ActiveAdmin::Axlsx::Builder
       def xlsx(options={}, &block)
+        return unless ActiveRecord::Base.connection.table_exists? config.resource_class.table_name
         config.xlsx_builder = ActiveAdmin::Axlsx::Builder.new(config.resource_class, options, &block)
       end
     end
